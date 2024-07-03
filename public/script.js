3d0ee615d2b3f3c1a534212c54cd3143e49dd1c3
Junstin Reh <notjun01@gmail.com>
2024-07-03 14:17:57 -0500

    moved buttons

--- public/script.js
+++ public/script.js
@@ -1,12 +1,17 @@
-import { gsap } from 'gsap';
+gsap.registerPlugin(ScrollTrigger)
 
 const card = document.getElementById('card');
-
-console.log("Before animation");
-gsap.to(card, { rotate: 360, duration: 1 });
-console.log("After animation");
-
+gsap.to(".card", {
+  x: 400,
+  rotate: 360,
+  duration: 1,
+  scrollTrigger: {
+    trigger: ".card", // Element to trigger the animation
+    start: "top center", // Start trigger when the top of the card reaches the center of the viewport
+    end: "bottom center", // End trigger when the bottom of the card reaches the center of the viewport
+    // markers: true // Show markers for debug purposes (optional)
+  }
+});
 
 const intro = document.getElementById('intro');
-
-gsap.to(intro, {rotate: 360, duration: 1});
+gsap.to(intro, { rotate: 360, duration: 1 });
